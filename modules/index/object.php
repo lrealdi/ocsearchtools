f3cf9e6e094250a6eebc5d80157fb6fd1e5de480
Luca Realdi <luca.realdi@opencontent.it>
2012-05-10 16:29:11 +0200

    fix modulo index (titoli e altre menatine)

--- modules/index/object.php
+++ modules/index/object.php
@@ -11,28 +11,46 @@ else
     $object = eZContentObject::fetch( intval( $ObjectID ) );
     if ( $object )
     {
-        echo "<h2>Indicizzazione di <em>" . $object->attribute( 'name' ) . "</em> main_node:  " . $object->attribute( 'main_node_id' ) . " classe: " . $object->attribute( 'class_identifier' ) . "</h2>";
+        echo "<h2>Indexing object ID: <em>" . $object->attribute( 'id' ) . "</em><br />Name: <em>" . $object->attribute( 'name' ) . "</em><br /> Main node ID:  <em>" . $object->attribute( 'main_node_id' ) . "</em><br />Class: <em>" . $object->attribute( 'class_identifier' ) . "</em></h2>";
         $attribues = $object->dataMap();
-        echo '<table>';
+        echo '<table cellpadding="10">';
+        echo '<tr>';
+        echo '<th>Identifier</th>';
+        echo '<th>Metadata</th>';
+        echo '<th>ezfSolrDocumentFieldBase::getData</th>';
+        echo '<tr>';
         foreach( $attribues as $i => $a )
         {
-            echo '<tr>';
-            echo '<td>';
-            echo $i . '<br /><small>(' . $a->attribute( 'data_type_string' ) . ')</small>';
-            echo '</td><td>';
-            var_dump( $a->metadata() );
-            echo '</td><td>';
-            $documentFieldBase = ezfSolrDocumentFieldBase::getInstance( $a );
-             echo '</td><td>';
-            var_dump( $documentFieldBase->getData() ) ;
-             echo '</td>';
-            echo '</tr>';
+            if ( $a->attribute( 'contentclass_attribute' )->attribute( 'is_searchable' ) > 0 )
+            {
+                echo '<tr>';
+                echo '<td>';
+                echo $i . '<br /><small>(' . $a->attribute( 'data_type_string' ) . ')</small>';
+                echo '</td><td>';
+                var_dump( $a->metadata() );
+                echo '</td><td>';
+                $documentFieldBase = ezfSolrDocumentFieldBase::getInstance( $a );
+                var_dump( $documentFieldBase->getData() ) ;
+                echo '</td>';
+                echo '</tr>';
+            }
+            else
+            {
+                echo '<tr>';
+                echo '<td>';
+                echo $i . '<br /><small>(' . $a->attribute( 'data_type_string' ) . ')</small>';
+                echo '</td><td>';
+                var_dump( $a->metadata() );
+                echo '</td><td>';
+                echo '<small><em>(not searchable)</em></small>';
+                echo '</td>';
+                echo '</tr>';
+            }
         }
         echo '</table>';
     
         $result = $searchEngine->addObject( $object, true );            
-        echo '<h2>Risultato indicizzazione: </h2>';
-        var_dump( $result );    
+        echo '<h2>Index result: ' . var_export( $result, 1 ) . '</h2>';
     }
     else
     {
