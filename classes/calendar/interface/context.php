a159f108489cd9608ade7aa56637ad82e7ec828e
OpenContent <info@opencontent.it>
2015-05-21 12:35:33 +0200

    Temp

--- classes/calendar/interface/context.php
+++ classes/calendar/interface/context.php
@@ -6,9 +6,13 @@ interface OCCalendarSearchContextInterface
 
     public function solrFetchParams();
 
+    public function taxonomiesCacheKey();
+    
     public function cacheKey();
 
     public function parseResults( array $rawResults, DateTime $startDateTime, DateTime $endDateTime = null );
+
+    public function parseFacets( array $rawFacetsFields, array $parsedRequest );
 
     public function getSolrFilters( array $data, OCCalendarSearchTaxonomy $taxonomy );
 
--- classes/calendar/occalendarsearchcontext.php
+++ classes/calendar/occalendarsearchcontext.php
@@ -15,6 +15,8 @@ abstract class OCCalendarSearchContext implements OCCalendarSearchContextInterfa
     
     public $forceTaxonomyIdentifiers = array();
     
+    public $parsedRequest = array();
+    
     final public static function instance( $contextIdentifier, $contextParameters = array(), $request = array() )
     {
         $ini = eZINI::instance( 'ocsearchtools.ini' );
@@ -158,5 +160,100 @@ abstract class OCCalendarSearchContext implements OCCalendarSearchContextInterfa
             }
         }
         return $item;
+    }
+    
+    protected function parseFacetItem( $currentItem, $currentFacetsResult )
+    {
+        $facetItem = false;
+        if ( array_key_exists( $currentItem['id'], $currentFacetsResult ) )
+        {
+            $facetItem = $currentItem;
+            $facetItem['count'] = $currentFacetsResult[$currentItem['id']];
+            $facetItem['is_selectable'] = 1;
+            $facetItem['children'] = array();
+            if ( $currentItem['children'] > 0 )
+            {
+                foreach( $currentItem['children'] as $child )
+                {
+                    $childItem = $this->parseFacetItem( $child, $currentFacetsResult );
+                    if ( $childItem )
+                    {
+                        $facetItem['children'][] = $childItem;
+                    }
+                }
+            }
+        }
+        if ( $currentItem['children'] > 0 )
+        {
+            $foundChildren = array();
+            foreach( $currentItem['children'] as $child )
+            {
+                $childItem = $this->parseFacetItem( $child, $currentFacetsResult );
+                if ( $childItem )
+                {
+                    $foundChildren[] = $childItem;
+                }
+            }
+            if ( count( $foundChildren ) > 0 )
+            {
+                $facetItem = $currentItem;
+                $facetItem['children'] = $foundChildren;
+                $facetItem['is_selectable'] = 1;
+            }
+        }
+        return $facetItem;
+    }
+
+    public function parseFacets( array $rawFacetsFields, array $parsedRequest )
+    {        
+        $currentFacetsResult = array();
+        foreach( $rawFacetsFields as $resultFacetGroup )
+        {
+            $currentFacetsResult = $currentFacetsResult + $resultFacetGroup['countList'];
+        }
+        $facets = array();
+        $taxonomyIdentifiers = array( 'what', 'where', 'target', 'category' );
+        $forceTaxonomyIdentifiers = $this->forceTaxonomyIdentifiers;
+        foreach( $taxonomyIdentifiers as $taxonomyIdentifier )
+        {
+            $taxonomy = OCCalendarSearchTaxonomy::instance( $taxonomyIdentifier, $this );
+            if ( $taxonomy instanceof OCCalendarSearchTaxonomy )
+            {
+                $facets[$taxonomyIdentifier] = array();
+                foreach( $taxonomy->getTree() as $item )
+                {
+                    $facetItem = $this->parseFacetItem( $item, $currentFacetsResult );
+                    if( in_array( $taxonomyIdentifier, $forceTaxonomyIdentifiers ) )
+                    {
+                        $item['is_selectable'] = $facetItem != false;
+                        $facets[$taxonomyIdentifier][] = $item;
+                    }
+                    elseif ( $facetItem )
+                    {
+                        $item['is_selectable'] = 1;
+                        $facets[$taxonomyIdentifier][] = $facetItem;
+                    }
+                    elseif ( isset( $parsedRequest[ $taxonomyIdentifier ] ) && in_array( $item['id'], $parsedRequest[ $taxonomyIdentifier ] ) )
+                    {
+                        $children = array();
+                        if ( $item['children'] > 0 )
+                        {
+                            foreach( $item['children'] as $child )
+                            {
+                                $childItem = $this->parseFacetItem( $child, $currentFacetsResult );
+                                if ( $childItem )
+                                {
+                                    $children[] = $childItem;
+                                }
+                            }
+                            $item['children'] = $children;
+                        }
+                        $item['is_selectable'] = 1;
+                        $facets[$taxonomyIdentifier][] = $item;
+                    }                    
+                }
+            }
+        }
+        return $facets;
     }
 }
